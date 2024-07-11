67af1f6e0d2a7e617ec83d7fa0c20ea6d9e20ff1
AlexDocking <AlexDocking@users.noreply.github.com>
2024-07-11 12:26:24 +0100

    Rename to GeometricMeanBenefitFunction and move inputs to constructor parameters

---
+++ XP Benefits/src/Mods/UserCode/XP Benefits/Core/Benefit Functions/GeometricMeanBenefitFunction.cs
@@ -0,0 +1,88 @@
+﻿//XP Benefits
+//Copyright (C) 2023 Alex Docking
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using Eco.Gameplay.Players;
+using Eco.Shared.Localization;
+using Eco.Shared.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XPBenefits
+{
+    /// <summary>
+    /// Scale the benefit by the geometric mean of the scaled inputs (inputs scaled to its range).
+    /// This requires all scaled inputs to be greater than zero to give any benefit.
+    /// </summary>
+    public class GeometricMeanBenefitFunction : IBenefitFunction, IBenefitInputDescriber
+    {
+        public BenefitValue MaximumBenefit { get; set; }
+        private List<IBenefitFunctionInput> Inputs { get; set; }
+        private List<InputDescriber> InputDescribers { get; set; }
+
+        public GeometricMeanBenefitFunction(IEnumerable<IBenefitFunctionInput> inputs, IEnumerable<InputDescriber> inputDescribers, BenefitValue maximumBenefit)
+        {
+            Inputs = new List<IBenefitFunctionInput>(inputs);
+            InputDescribers = new List<InputDescriber>(inputDescribers);
+            if (Inputs.Count != InputDescribers.Count) throw new ArgumentException($"Must be same number of describers as inputs ({Inputs.Count} inputs vs {InputDescribers.Count} describers)");
+            MaximumBenefit = maximumBenefit;
+        }
+
+        public float CalculateBenefit(User user)
+        {
+            try
+            {
+                float product = Inputs.Select(input => input.GetScaledInput(user)).Mult();
+                float fractionOfBenefitToApply = (float)Math.Pow(product, 1f / Inputs.Count);
+                return fractionOfBenefitToApply * MaximumBenefit.GetValue(user);
+            }
+            catch
+            {
+            }
+            return 0;
+        }
+
+        #region IBenefitInputDescriber
+
+        public IBenefitInputDescriber Describer => this;
+
+        public LocString InputName(User user) => Localizer.Do($"{InputDescribers.Select(describer => (describer as IBenefitInputDescriber).InputName(user)).CommaList()} {Localizer.PluralNoNum("multiplier")}");
+
+        public LocString MeansOfImprovingStat(User user) => Localizer.Do($"You can increase this benefit by improving your {InputDescribers.Select(describer => (describer as IBenefitInputDescriber).InputName(user)).CommaList()} {Localizer.PluralNoNum("multiplier")}. If you want to see the greatest improvement you should improve the lowest percentage first. {InputDescribers.Select(describer => describer.AdditionalInfo).Where(s => s.IsSet()).JoinList(". ")}");
+
+        public LocString MaximumInput(User user)
+        {
+            List<LocString> locs = new List<LocString>();
+            for (int i = 0; i < Inputs.Count; i++)
+            {
+                locs.Add(Localizer.Do($"{Text.StyledNum(Inputs[i].GetInputRange(user).Max)} {InputDescribers[i].InputName}"));
+            }
+            return locs.CommaList();
+        }
+
+        public LocString CurrentInput(User user)
+        {
+            List<LocString> locs = new List<LocString>();
+            for (int i = 0; i < Inputs.Count; i++)
+            {
+                locs.Add(Localizer.Do($"{Text.GradientColoredPercent(Inputs[i].GetScaledInput(user))} {InputDescribers[i].InputName}"));
+            }
+            return locs.CommaList();
+        }
+
+        #endregion IBenefitInputDescriber
+    }
+}
--- XP Benefits/src/Mods/UserCode/XP Benefits/Core/Benefit Functions/GeometricMeanFoodHousingBenefitFunction.cs
+++ XP Benefits/src/Mods/UserCode/XP Benefits/Core/Benefit Functions/GeometricMeanFoodHousingBenefitFunction.cs
@@ -15,13 +15,9 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using Eco.Core.Plugins.Interfaces;
 using Eco.Gameplay.EcopediaRoot;
-using Eco.Gameplay.Players;
 using Eco.Gameplay.Systems.TextLinks;
 using Eco.Shared.Localization;
-using Eco.Shared.Utils;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace XPBenefits
 {
@@ -33,27 +29,16 @@ namespace XPBenefits
         }
     }
 
-    /// <summary>
-    /// Scale the benefit by the amount of food and housing xp the player has
-    /// in such a way as to require both sources of xp to give any benefit
-    /// </summary>
-    public class GeometricMeanFoodHousingBenefitFunction : IBenefitFunction, IBenefitInputDescriber
+    public class GeometricMeanFoodHousingBenefitFunctionFactory : IBenefitFunctionFactory
     {
-        public XPConfig XPConfig { get; set; }
-        public bool XPLimitEnabled { get; set; }
-        public BenefitValue MaximumBenefit { get; set; }
-        private List<IBenefitFunctionInput> Inputs { get; set; }
-        private List<InputDescriber> InputDescribers { get; set; }
+        public string Name => "GeometricMeanFoodHousing";
+        public string Description => "Uses a combination of the amount of food and housing xp the player has in such a way as to require both sources of xp to give any benefit.";
 
-        public GeometricMeanFoodHousingBenefitFunction(XPConfig xpConfig, BenefitValue maximumBenefit, bool xpLimitEnabled = false)
+        public IBenefitFunction Create(XPConfig xpConfig, BenefitValue maximumBenefit, bool xpLimitEnabled = false)
         {
-            XPConfig = xpConfig;
-            XPLimitEnabled = xpLimitEnabled;
-            MaximumBenefit = maximumBenefit;
-
             FoodXPInput foodInput = new FoodXPInput(xpConfig);
             HousingXPInput housingInput = new HousingXPInput(xpConfig);
-            Inputs = new List<IBenefitFunctionInput>()
+            List<IBenefitFunctionInput> inputs = new List<IBenefitFunctionInput>()
             {
                 foodInput,
                 housingInput
@@ -73,66 +58,12 @@ namespace XPBenefits
                 InputTitle = housingInputTitle,
                 MeansOfImprovingStatDescription = Localizer.Do($"You can increase this benefit by improving your {housingInputTitle}"),
             };
-            InputDescribers = new List<InputDescriber>()
+            List<InputDescriber> inputDescribers = new List<InputDescriber>()
             {
                 foodInputDescriber,
                 housingInputDescriber
             };
-        }
-
-        public float CalculateBenefit(User user)
-        {
-            try
-            {
-                float product = Inputs.Select(input => input.GetScaledInput(user)).Mult();
-                float fractionOfBenefitToApply = (float)Math.Pow(product, 1f / Inputs.Count);
-                return fractionOfBenefitToApply * MaximumBenefit.GetValue(user);
-            }
-            catch
-            {
-            }
-            return 0;
-        }
-
-        #region IBenefitInputDescriber
-
-        public IBenefitInputDescriber Describer => this;
-
-        public LocString InputName(User user) => Localizer.Do($"{InputDescribers.Select(describer => (describer as IBenefitInputDescriber).InputName(user)).CommaList()} {Localizer.PluralNoNum("multiplier")}");
-
-        public LocString MeansOfImprovingStat(User user) => Localizer.Do($"You can increase this benefit by improving your {InputDescribers.Select(describer => (describer as IBenefitInputDescriber).InputName(user)).CommaList()} {Localizer.PluralNoNum("multiplier")}. If you want to see the greatest improvement you should improve the lowest percentage first. {InputDescribers.Select(describer => describer.AdditionalInfo).Where(s => s.IsSet()).JoinList(". ")}");
-
-        public LocString MaximumInput(User user)
-        {
-            List<LocString> locs = new List<LocString>();
-            for (int i = 0; i < Inputs.Count; i++)
-            {
-                locs.Add(Localizer.Do($"{Text.StyledNum(Inputs[i].GetInputRange(user).Max)} {InputDescribers[i].InputName}"));
-            }
-            return locs.CommaList();
-        }
-
-        public LocString CurrentInput(User user)
-        {
-            List<LocString> locs = new List<LocString>();
-            for (int i = 0; i < Inputs.Count; i++)
-            {
-                locs.Add(Localizer.Do($"{Text.GradientColoredPercent(Inputs[i].GetScaledInput(user))} {InputDescribers[i].InputName}"));
-            }
-            return locs.CommaList();
-        }
-
-        #endregion IBenefitInputDescriber
-    }
-
-    public class GeometricMeanFoodHousingBenefitFunctionFactory : IBenefitFunctionFactory
-    {
-        public string Name => "GeometricMeanFoodHousing";
-        public string Description => "Uses a combination of the amount of food and housing xp the player has in such a way as to require both sources of xp to give any benefit.";
-
-        public IBenefitFunction Create(XPConfig xpConfig, BenefitValue maximumBenefit, bool xpLimitEnabled = false)
-        {
-            return new GeometricMeanFoodHousingBenefitFunction(xpConfig, maximumBenefit, xpLimitEnabled);
+            return new GeometricMeanBenefitFunction(inputs, inputDescribers, maximumBenefit);
         }
     }
 }
